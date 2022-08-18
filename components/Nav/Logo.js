774cf256b2b2cd1a7175207a3c6c6744b16264cf
Pratham Saraf <prathammsaraf@gmail.com>
2022-08-19 04:21:24 +0530

    added icon change on changing theme

--- components/Nav/Logo.js
+++ components/Nav/Logo.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { useContext } from "react";
 import styled, { ThemeContext } from "styled-components";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 
 const Container = styled(motion.div)`
   width: 100%;
@@ -12,12 +12,14 @@ const Container = styled(motion.div)`
 
 export default function Logo({ color, variants = {} }) {
   const theme = useContext(ThemeContext);
-    const themeUpdate = useThemeMode();
+  const themeUpdate = useThemeMode();
+
+  const isDarkMode = theme.name.current === "dark"? true : false;
 
   return (
     <>
-    <button onClick={() => themeUpdate.toggleMode()} id="theme-toggle">
-    <FontAwesomeIcon icon={faMoon} size="lg"/>
+    <button onClick={() => themeUpdate.toggleMode()}>
+    {isDarkMode ? <FontAwesomeIcon icon={faSun} fontSize="2em" data-toggle="tooltip" data-placement="left" title="Toggle to Light Mode"/> : <FontAwesomeIcon icon={faMoon} fontSize="2em" data-toggle="tooltip" data-placement="left" title="Toggle to Dark Mode"/>}
     </button>
     </>
   );
